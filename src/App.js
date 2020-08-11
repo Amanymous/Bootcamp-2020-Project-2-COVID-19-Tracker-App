d814d9d15f49c1f7fd09fbfef5e3f0b303639d23
aman mirza <aman mirza99>
2020-08-11 11:54:12 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Covid-19 Tracker</h1>
+      <h1>Covid-19 Tracker...</h1>
     </div>
   );
 }
